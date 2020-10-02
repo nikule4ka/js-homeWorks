cfce6c4fcb53ed57a0bbbdf1a15ea148e4c1d296
nikule4ka <64929516+nikule4ka@users.noreply.github.com>
2020-10-02 14:01:30 +0300

    task5

---
+++ js/task5.js
@@ -0,0 +1,40 @@
+const countryName = "Ямайка";
+
+const CANCELED_BY_USER = "Отменено пользователем!";
+const NO_DELIVERY = "В выбранную страну доставка недоступна.";
+const CHINA = "Китай";
+const AUSTRALIA = "Австралия";
+const INDIA = "Индия";
+const JAMAICA = "Ямайка";
+let message;
+let price = 0;
+let country;
+
+if (countryName === null) {
+  message = CANCELED_BY_USER;
+} else {
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
+  switch (country) {
+    case CHINA:
+      price = 100;
+      break;
+    case AUSTRALIA:
+      price = 170;
+      break;
+    case INDIA:
+      price = 80;
+      break;
+    case JAMAICA:
+      price = 120;
+      break;
+    default:
+      message = NO_DELIVERY;
+  }
+}
+if (price > 0) {
+  message = `Доставка в ${country} будет стоить ${price} кредитов`;
+} else {
+  message = NO_DELIVERY;
+}
+
+console.log(message);
