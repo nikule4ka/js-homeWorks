4c55c1ca5f7a3db753b5f876a71697412c194254
nikule4ka <64929516+nikule4ka@users.noreply.github.com>
2020-10-03 13:06:47 +0300

    create task2

---
+++ js/task2.js
@@ -0,0 +1,11 @@
+// const calculateEngravingPrice = function (message = "", pricePerWord = 0) {
+//   const calc = message.split(" ").length;
+//   return calc * pricePerWord;
+// };
+
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     10
+//   )
+// ); // 80
