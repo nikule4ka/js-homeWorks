5a8d9abb144deab948a7c9720d9371fe35432f39
nikule4ka <64929516+nikule4ka@users.noreply.github.com>
2020-10-03 13:45:22 +0300

    task4

---
+++ js/task4.js
@@ -0,0 +1,14 @@
+function formatString(string, maxLength = 40) {
+  const sentence = string.length < maxLength;
+
+  return sentence ? string : string.slice(0, maxLength) + "...";
+}
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+// 'Vestibulum facilisis, purus ne...'
