2b08b1c31b350a4a187fbb8d5a884454867b8d1b
nikule4ka <64929516+nikule4ka@users.noreply.github.com>
2020-11-01 12:14:06 +0200

    task1

--- js/task1.js
+++ js/task1.js
@@ -1,24 +1,34 @@
-const isUniq = (element, index, arr) => arr.indexOf(element) === index;
-const isEven = (element) => element % 2 === 0;
+// Write code under this line
 
-function filterArray(array, cb) {
-  'use strict';
-  const numbers = [];
-  for(let i = 0; i < array.length; i += 1) {
+const addIndex = (element, index) => element + index;
+// Write code under this line
+
+const subIndex = (element, index) => element - index;
+
+function mapArray(array, cb) {
+  "use strict";
+  // Write code under this line
+  const numbers = new Array(array.length);
+  for (let i = 0; i < array.length; i += 1) {
     const element = array[i];
     const index = i;
-    // Write code under this line
-   if (cb(element, index, array)) {
-   numbers.push(array[i]);
-   }
+    numbers[i] = cb(element, index);
   }
   return numbers;
 }
 
-const arr  = [1,2,3,4,5,1,2,5];
+const arr = [1, 2, 3, 4, 5];
 
-console.log(filterArray(arr, isUniq));
-// [1, 2, 3, 4, 5]
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-console.log(filterArray(arr, isEven));
-// [2, 4, 2]
+console.log(mapArray(arr, addIndex));
+// [1, 3, 5, 7, 9]
+
+console.log(mapArray(arr, subIndex));
+// [1, 1, 1, 1, 1]
+
+console.log(mapArray(arr1, addIndex));
+// [1, 3, 5, 7, 9]
+
+console.log(mapArray(arr1, subIndex));
+// [1, 1, 1, 1, 1]
